8066b31e1ad485807459da5a452471efdc7dd1ea
Itiberê Gonçalves Silva Filho <56208580+Itibere10@users.noreply.github.com>
2023-11-06 14:29:17 -0300

    Update choco-install.bat

--- choco-install.bat
+++ choco-install.bat
@@ -1,1 +1,1 @@
-choco install microsoft-windows-terminal steam epicgameslauncher discord spotify git vscode python orwelldevcpp -r -y
+choco install microsoft-windows-terminal steam epicgameslauncher discord spotify git vscode python orwelldevcpp winrar -r -y
